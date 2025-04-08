9aed36b3b65d1fdbeaaed6d55d1f360c23adf1b0
Joy Janney <143513601+janneyj@users.noreply.github.com>
2025-04-08 15:03:52 -0400

    Class Files
    Added the class files with default values that will need to change

---
+++ Eyes.java
@@ -0,0 +1,9 @@
+package application;
+class Eyes extends BodyParts
+{
+	String type;
+	String name;
+	static int xAxis = 15;
+	static int yAxis = 15;
+	String filePath = FileOrg.findFile("eye01");
+}
